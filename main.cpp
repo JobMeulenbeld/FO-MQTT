c8106398d92af6f032c166a9b4b1a9c193c69640
JobMeulenbeld <meulenbeldjob@gmail.com>
2023-05-06 13:46:49 +0200

    Updated library usage

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 
 int main(int argc, char* argv[])
 {
-    MQTT_Handler handler("tcp://192.168.68.107:1883", "Job Meulenbeld");
+    MQTT_Handler handler("tcp://192.168.68.107:1883", "FO-MQTT");
 
     while(true){
         handler.publish("Topic/World", "World", 5);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 }
